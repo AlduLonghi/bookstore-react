d32e06e4d3df9241fe563b180cfe3b653bc15987
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-01-26 10:15:21 -0300

    Add handleFilterChange method

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -2,29 +2,38 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../components/Book';
 import { removeBook } from '../redux/actions/index';
+import CategoryFilter from '../components/CategoryFilter';
+import filterReducer from '../redux/reducers/filters';
 
-const BooksList = ({ books, removeBook }) => {
+const BooksList = ({ books, removeBook, filterReducer }) => {
   const handleRemoveBook = bookId => {
     removeBook(bookId);
   };
 
+  const handleFilterChange = e => {
+    filterReducer(e.target.value);
+  };
+
   return (
-    <table>
-      <tr>
-        <th>Book ID</th>
-        <th>Title</th>
-        <th>Category</th>
-      </tr>
-      {books.map(book => (
-        <Book
-          key={book.id}
-          bookId={book.id}
-          bookTitle={book.title}
-          bookCategory={book.category}
-          onClick={handleRemoveBook}
-        />
-      ))}
-    </table>
+    <div>
+      <table>
+        <tr>
+          <th>Book ID</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
+        {books.map(book => (
+          <Book
+            key={book.id}
+            bookId={book.id}
+            bookTitle={book.title}
+            bookCategory={book.category}
+            onClick={handleRemoveBook}
+          />
+        ))}
+      </table>
+      <CategoryFilter />
+    </div>
   );
 };
 
@@ -39,11 +48,12 @@ BooksList.defaultProps = {
 };
 
 const mapStateToProps = state => ({
-  books: state.booksReducer,
+  books: state.booksReducer.books,
 });
 
 const mapDispatchToProps = {
   removeBook,
+
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(BooksList);
--- src/redux/reducers/filters.js
+++ src/redux/reducers/filters.js
@@ -3,8 +3,10 @@ const CHANGE_FILTER = 'CHANGE_FILTER';
 const filterReducer = (state, action) => {
   switch (action.payload) {
     case ('Action' || 'Biography' || 'History' || 'Horror' || 'Kids' || 'Learning' || 'Sci-Fi'):
-      return state.filter = action.payload;
+      return {...state, filter: action.payload};
     default:
       return state.filter;
   }
 };
+
+export default filterReducer;
