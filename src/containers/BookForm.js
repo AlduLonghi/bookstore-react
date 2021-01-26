01eb1e73fe4947489f29586cfded358367c6aa7d
Aldana Abril Longhi <aldulonghi@gmail.com>
2021-01-26 07:28:55 -0300

    Refactor BookForm component using hooks

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,62 +1,43 @@
 import { connect } from 'react-redux';
-import { Component } from 'react';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import { createBook } from '../redux/actions';
 
-class BookForm extends Component {
-  constructor({ createBook }) {
-    super(createBook);
+const BookForm = ({ createBook }) => {
+  const [book, setBook] = useState({ title: '', category: '' });
 
-    this.state = {
-      title: '',
-      category: '',
-      id: null,
-    };
+  const handleChange = e => {
+    setBook({ ...book, [e.target.name]: e.target.value });
+  };
 
-    this.createBook = createBook;
+  const handleSubmit = () => {
+    const newBook = { ...book, id: Math.random() };
+    createBook(newBook);
+    setBook({ title: '', category: '' });
+  };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
-  handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  handleSubmit() {
-    const newBook = { ...this.state, id: Math.random() };
-    this.createBook(newBook);
-
-    this.setState({
-      title: '',
-      category: '',
-      id: null,
-    });
-  }
-
-  render() {
-    const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-    return (
-      <form>
-        <div>
-          <label htmlFor="title">
-            Title of your book:
-            <input type="text" id="book" name="title" onChange={this.handleChange} />
-          </label>
-        </div>
-        <div>
-          <label htmlFor="booksCategory">
-            Choose a category:
-            <select name="category" id="category" onChange={this.handleChange}>
-              {bookCategories.map(categ => <option key={categ} value={categ}>{categ}</option>)}
-            </select>
-          </label>
-        </div>
-        <button type="button" onClick={this.handleSubmit}>Submit</button>
-      </form>
-    );
-  }
-}
+  return (
+    <form>
+      <div>
+        <label htmlFor="title">
+          Title of your book:
+          <input type="text" id="book" name="title" onChange={handleChange} />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="booksCategory">
+          Choose a category:
+          <select name="category" id="category" onChange={handleChange}>
+            {bookCategories.map(categ => <option key={categ} value={categ}>{categ}</option>)}
+          </select>
+        </label>
+      </div>
+      <button type="button" onClick={handleSubmit}>Submit</button>
+    </form>
+  );
+};
 
 BookForm.defaultProps = {
   createBook: null,
